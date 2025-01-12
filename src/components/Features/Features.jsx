bc9134a8ce2277b27e8b92ae2c8a7993ef8d89c6
Yuri <wall.mart.boss@proton.me>
2025-01-12 03:12:42 +0200

    fix features

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -8,34 +8,34 @@ const Features = () => {
       <div className={s.icons}>
         <PropertyIcons camper={camper} />
       </div>
-      <div className={s.info}>
+      <ul className={s.info}>
         <p className={s.title}>Vehicle details</p>
         <hr className={s.line} />
-        <div className={s.item}>
+        <li className={s.item}>
           <p className={s.text}>form</p>
           <p className={s.text}>{camper?.form}</p>
-        </div>
-        <div className={s.item}>
+        </li>
+        <li className={s.item}>
           <p className={s.text}>length</p>
           <p className={s.text}>{camper?.length}</p>
-        </div>
-        <div className={s.item}>
+        </li>
+        <li className={s.item}>
           <p className={s.text}>width</p>
           <p className={s.text}>{camper?.width}</p>
-        </div>
-        <div className={s.item}>
+        </li>
+        <li className={s.item}>
           <p className={s.text}>height</p>
           <p className={s.text}>{camper?.height}</p>
-        </div>
-        <div className={s.item}>
+        </li>
+        <li className={s.item}>
           <p className={s.text}>tank</p>
           <p className={s.text}>{camper?.tank}</p>
-        </div>
-        <div className={s.item}>
+        </li>
+        <li className={s.item}>
           <p className={s.text}>consumption</p>
           <p className={s.text}>{camper?.consumption}</p>
-        </div>
-      </div>
+        </li>
+      </ul>
     </div>
   );
 };
